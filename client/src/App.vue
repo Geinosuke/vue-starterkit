55d9fc4908f79b28a6aa2a5542b80de5d492329b
hermann.diakite <hermann.diakite@epita.fr>
2018-02-07 17:31:24 +0100

    [UPDATE] README

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,9 +4,9 @@
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
     <ul>
+      <li><a href="https://github.com/Geinosuke/vue-starterkit" target="_blank">Github</a></li>
       <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
       <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
     </ul>
     <h2>Ecosystem</h2>
@@ -33,7 +33,7 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App',
+      msg: 'Welcome my Vue.js App staterkit',
       tests: []
     }
   },
